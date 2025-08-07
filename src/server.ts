29a0a1fef8dce1cf59da866015318d49c61e00ba
Ali Muksin <ali.muksin0510@gmail.com>
2025-08-07 11:37:07 +0700

    simple create user with i/o operaton

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import exprress, { Request, Response, Application } from "express";
 import bodyParser from "body-parser";
 import crypto from "crypto";
-import fileSystem from "fs";
+import { promises as fs } from "fs";
+import path from "path";
+import * as Yup from "yup";
 import dotenv from "dotenv";
 
 // config dotenv
@@ -38,10 +40,10 @@ const users: Array<{ id: string; name: string }> = [];
 // Query: none -> /users
 // Method: GET
 // Description: Retrieve all users
-app.get("/users", (req: Request, res: Response) => {
+app.get("/users", async (req: Request, res: Response) => {
   // TODO: implement filtering by name
   // READ JSON file from (json/data.json)
-  const RAW_DATA = fileSystem.readFileSync("json/data.json", "utf-8");
+  const RAW_DATA = await fs.readFile("json/data.json", "utf-8");
   const DATA = JSON.parse(RAW_DATA);
 
   res.status(200).json({
@@ -77,18 +79,90 @@ app.get("/users/:id", (req: Request, res: Response) => {
   });
 });
 
-// POST: create a new user
-app.post("/users", (req: Request, res: Response) => {
-  users.push({
-    id: crypto.randomUUID(),
-    name: req.body.name,
-  });
+// POST: register a new user
+// URL: /users
+// Body: { "name": "John Doe", "email": "", "password": "plain text" }
+// Method: POST
+// Description: Create a new user
+const RegisterSchema = Yup.object().shape({
+  name: Yup.string().required("Name is required"),
+  email: Yup.string()
+    .email("Invalid email format")
+    .required("Email is required"),
+  password: Yup.string()
+    .min(6, "Password must be at least 6 characters")
+    .required("Password is required"),
+});
+app.post("/users", async (req: Request, res: Response) => {
+  try {
+    // Validate request body -> Yup
+    await RegisterSchema.validate(req.body, { abortEarly: false });
 
-  res.status(201).json({
-    success: true,
-    message: "User created successfully",
-    data: users[users.length - 1],
-  });
+    // Check data validation on current JSON file (email & name unique)
+    const FILE_PATH = path.join(__dirname, "../json/data.json");
+    const RAW_DATA = await fs.readFile(FILE_PATH, "utf-8");
+    const DATA = JSON.parse(RAW_DATA);
+
+    // Check if email already exists
+    const existingUser = DATA.users.find(
+      (user: { email: string }) => user.email === req.body.email,
+    );
+    if (existingUser) {
+      throw new Error("Email already exists");
+    }
+
+    // create new user -> provide UID, plain text password, set createdAt and updatedAt
+    const newUser = {
+      id: crypto.randomUUID(),
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password, // In a real application, hash the password before saving
+      actice: true, // Default status true
+      role: "user", // Default role
+      createdAt: new Date().toISOString(),
+      updatedAt: new Date().toISOString(),
+    };
+
+    // update JSON file (json/data.json)
+    DATA.users.push(newUser);
+    await fs.writeFile("json/data.json", JSON.stringify(DATA, null, 2));
+
+    // Respond with success
+    res.status(201).json({
+      success: true,
+      message: "User created successfully",
+      data: newUser,
+    });
+  } catch (error) {
+    if (error instanceof Yup.ValidationError) {
+      return res.status(400).json({
+        success: false,
+        message: "Validation error",
+        errors: error.errors,
+      });
+    } else if (error instanceof Error) {
+      return res.status(400).json({
+        success: false,
+        message: error.message,
+        data: {},
+      });
+    }
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+      data: {},
+    });
+  }
+  // users.push({
+  //   id: crypto.randomUUID(),
+  //   name: req.body.name,
+  // });
+
+  // res.status(201).json({
+  //   success: true,
+  //   message: "User created successfully",
+  //   data: users[users.length - 1],
+  // });
 });
 
 // PUT/PATCH: update a user
